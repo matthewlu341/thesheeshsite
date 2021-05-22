a4f1c41a86d1f962e050be6ac228a450dc889325
Matthew Lu <matthew.lu341@gmail.com>
2021-05-22 18:12:01 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function soundHandler(sound){
+    if(sound == 'sheesh'){
+        let sheesh = new Audio("sheesh.mp3");
+        sheesh.play();
+    } else {
+        let ahlie = new Audio("ahlie.mp3");
+        ahlie.play();
+    }
+}
